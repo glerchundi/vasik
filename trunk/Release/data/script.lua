2a391edd484b411acd0fce2e3c4ffaf876e670c9
glertxundi <glertxundi@0aa467b6-4590-11de-a2ae-350cbd0950bb>
2009-06-12 17:31:36 +0000

    First usable release?, not for a major,minor,revision version yet....

--- trunk/Release/data/script.lua
+++ trunk/Release/data/script.lua
@@ -7,39 +7,51 @@ fxcubes     = demosystem.effect();
 scheduler   = demosystem.scheduler();
 timer       = demosystem.timer();
 font        = demosystem.font();
+shell       = demosystem.shell();
 
 -- INIT WINDOW
-window:init('Vasik*',640,480,32,false);
+window:init('Vasik*',800,600,32,false);
 
 -- INIT SOUNDSYSTEM
 soundsys:init();
-soundsys:spectrum(true);
 soundsys:loadstream("oasis","data/Oasis - Wonderwall.mp3");
-soundsys:playstream("oasis");
+--soundsys:playstream("oasis");
 
 -- INIT TIMER
 timer:init();
 
 -- INIT FONT
+font:initplugin("window", window);
 font:init();
-font:setFont("data/Andale Mono.ttf",12);
+font:setFont("data/Fixedsys500c.ttf",10);
+
+-- INIT SHELL
+shell:initplugin("window", window);
+shell:initplugin("timer", timer);
+shell:initplugin("font", font);
+shell:init();
 
 -- INIT EFFECTS
-fxcubes:init();
 fxcubes:initplugin("window", window);
 fxcubes:initplugin("timer", timer);
 fxcubes:initplugin("font", font);
+fxcubes:init();
 
 -- INIT SCHEDULER
-scheduler:init(1);
 scheduler:initplugin("window", window);
 scheduler:initplugin("timer", timer);
 scheduler:initplugin("cubes", fxcubes);
+scheduler:initplugin("shell", shell);
+scheduler:init(1);
 
 scheduler:addfx(0,0,-1,"cubes","render", nil);
+scheduler:addfx(0,0,-1,"shell","render",nil);
 
 scheduler:play(0);
 
+shell:quit();
+font:quit();
+timer:quit();
 scheduler:quit();
 fxcubes:quit();
 soundsys:quit();
